ffe43c83f3bf3231977eeffe7aee277e7ede2322
Cahit Barkın Özer <43732258+cbarkinozer@users.noreply.github.com>
2023-07-13 13:52:58 +0300

    Update choose_location.dart

--- world_time/choose_location.dart
+++ world_time/choose_location.dart
@@ -8,6 +8,10 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +22,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('choose locaiton screen'),
+      body: ElevatedButton(onPressed: () {
+        setState(() {
+          counter+=1;
+        });
+      },
+        child: Text('The counter is $counter'),
+
+      ),
     );
   }
 }
